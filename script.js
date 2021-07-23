2da9e7fffb53fe6166059c51a118eb5c51f8d3a1
jyjiayi <sojiayi@gmail.com>
2021-07-23 14:18:13 +0800

    Added Square formatting

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var main = function (input) {
   var myOutputValue = "";
+
   for (var counter = 0; counter < input; counter += 1) {
-    myOutputValue += "👍";
+    for (var count = 0; count < input; count += 1) {
+      myOutputValue += "👍";
+    }
+    myOutputValue += `<br>`;
   }
 
   return myOutputValue;
