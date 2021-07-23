4c556c1c1d17753326b6c34cefe6bcb391f8213b
jyjiayi <sojiayi@gmail.com>
2021-07-23 14:21:20 +0800

    Changed to triangle formatting

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var main = function (input) {
   var myOutputValue = "";
+  var innerCount = 1;
 
   for (var counter = 0; counter < input; counter += 1) {
-    for (var count = 0; count < input; count += 1) {
+    for (var count = 0; count < innerCount; count += 1) {
       myOutputValue += "👍";
     }
+    innerCount += 1;
     myOutputValue += `<br>`;
   }
 
